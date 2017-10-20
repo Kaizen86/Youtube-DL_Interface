dd4cb70beca032202a61e5be4a3f49b454206e48
floathandthing <springtrap.bite87@gmail.com>
2017-10-20 10:04:43 +0100

    Prototype, only basic support for downloading/audio extraction. Needs labelling.

--- Youtube-DL_Interface/Form1.Designer.cs
+++ Youtube-DL_Interface/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Youtube_DL_Interface
 {
-    partial class Form1
+    partial class Window
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.start_download = new System.Windows.Forms.Button();
+            this.audioformat = new System.Windows.Forms.ComboBox();
+            this.audioquality = new System.Windows.Forms.NumericUpDown();
+            this.onlyaudio = new System.Windows.Forms.CheckBox();
+            this.targetExecutable = new System.Windows.Forms.TextBox();
+            this.urlinput = new System.Windows.Forms.TextBox();
+            this.cmdremainstate = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.audioquality)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start_download
+            // 
+            this.start_download.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.start_download.Location = new System.Drawing.Point(12, 334);
+            this.start_download.Name = "start_download";
+            this.start_download.Size = new System.Drawing.Size(438, 71);
+            this.start_download.TabIndex = 0;
+            this.start_download.Text = "START";
+            this.start_download.UseVisualStyleBackColor = true;
+            this.start_download.Click += new System.EventHandler(this.start_download_Click);
+            // 
+            // audioformat
+            // 
+            this.audioformat.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.audioformat.FormattingEnabled = true;
+            this.audioformat.Location = new System.Drawing.Point(320, 70);
+            this.audioformat.Name = "audioformat";
+            this.audioformat.Size = new System.Drawing.Size(121, 28);
+            this.audioformat.TabIndex = 1;
+            this.audioformat.Text = "mp3";
+            // 
+            // audioquality
+            // 
+            this.audioquality.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.audioquality.Location = new System.Drawing.Point(192, 71);
+            this.audioquality.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.audioquality.Name = "audioquality";
+            this.audioquality.Size = new System.Drawing.Size(120, 26);
+            this.audioquality.TabIndex = 2;
+            this.audioquality.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // onlyaudio
+            // 
+            this.onlyaudio.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.onlyaudio.AutoSize = true;
+            this.onlyaudio.Location = new System.Drawing.Point(12, 72);
+            this.onlyaudio.Name = "onlyaudio";
+            this.onlyaudio.Size = new System.Drawing.Size(181, 24);
+            this.onlyaudio.TabIndex = 3;
+            this.onlyaudio.Text = "Only download audio";
+            this.onlyaudio.UseVisualStyleBackColor = true;
+            // 
+            // targetExecutable
+            // 
+            this.targetExecutable.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.targetExecutable.Location = new System.Drawing.Point(12, 302);
+            this.targetExecutable.Name = "targetExecutable";
+            this.targetExecutable.Size = new System.Drawing.Size(100, 26);
+            this.targetExecutable.TabIndex = 4;
+            this.targetExecutable.Text = "youtube-dl";
+            // 
+            // urlinput
+            // 
+            this.urlinput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.urlinput.Location = new System.Drawing.Point(12, 12);
+            this.urlinput.Name = "urlinput";
+            this.urlinput.Size = new System.Drawing.Size(429, 26);
+            this.urlinput.TabIndex = 5;
+            // 
+            // cmdremainstate
+            // 
+            this.cmdremainstate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdremainstate.AutoSize = true;
+            this.cmdremainstate.Location = new System.Drawing.Point(129, 304);
+            this.cmdremainstate.Name = "cmdremainstate";
+            this.cmdremainstate.Size = new System.Drawing.Size(249, 24);
+            this.cmdremainstate.TabIndex = 6;
+            this.cmdremainstate.Text = "Keep Command Window open";
+            this.cmdremainstate.UseVisualStyleBackColor = true;
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(453, 408);
+            this.Controls.Add(this.cmdremainstate);
+            this.Controls.Add(this.urlinput);
+            this.Controls.Add(this.audioquality);
+            this.Controls.Add(this.audioformat);
+            this.Controls.Add(this.start_download);
+            this.Controls.Add(this.onlyaudio);
+            this.Controls.Add(this.targetExecutable);
+            this.Name = "Window";
+            this.Text = "Youtube-DL Interface";
+            ((System.ComponentModel.ISupportInitialize)(this.audioquality)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button start_download;
+        private System.Windows.Forms.ComboBox audioformat;
+        private System.Windows.Forms.NumericUpDown audioquality;
+        private System.Windows.Forms.CheckBox onlyaudio;
+        private System.Windows.Forms.TextBox targetExecutable;
+        private System.Windows.Forms.TextBox urlinput;
+        private System.Windows.Forms.CheckBox cmdremainstate;
     }
 }
 
--- Youtube-DL_Interface/Form1.cs
+++ Youtube-DL_Interface/Form1.cs
@@ -10,11 +10,70 @@ using System.Windows.Forms;
 
 namespace Youtube_DL_Interface
 {
-    public partial class Form1 : Form
+    public partial class Window : Form
     {
-        public Form1()
+        public Window()
         {
             InitializeComponent();
+
+            //Populate the audioformat list
+            audioformat.Items.Clear();
+            audioformat.Items.Add("best");
+            audioformat.Items.Add("aac");
+            audioformat.Items.Add("flac");
+            audioformat.Items.Add("mp3");
+            audioformat.Items.Add("m4a");
+            audioformat.Items.Add("opus");
+            audioformat.Items.Add("vorbis");
+            audioformat.Items.Add("wav");
+
+            //disable audioformat and audioquality on startup, so by default you download video.
+            audioformat.Enabled = false;
+            audioquality.Enabled = false;
         }
+        private void onlyaudio_CheckedChanged(object sender, EventArgs e)
+        {
+            //Enables/disables audio extraction options
+            if (onlyaudio.Checked)
+            {
+                audioformat.Enabled = true;
+                audioquality.Enabled = true;
+            }
+            else
+            {
+                audioquality.Enabled = false;
+                audioformat.Enabled = false;
+            }
+        }
+        //strings needed by start_download_Click()
+        public static string audioparam;
+        public static string cmdvanish;
+
+        private void start_download_Click(object sender, EventArgs e)
+        {
+            //Get the executable name. This is kinda important.
+            string targetprgname = targetExecutable.Text;
+            //Get the URL provided to us
+            string URL = urlinput.Text;
+
+            //build the command by adding parameters
+            if (onlyaudio.Checked) { audioparam = ("-x --audio-format " + audioformat.Text + " --audio-quality " + audioquality.Value); }
+            if (cmdremainstate.Checked) { cmdvanish = "/K"; } //If they want the window to remain open afterwards, use /K.
+            else { cmdvanish = "/C"; } //Otherwise /C.
+
+            //This is the generated command from the data.
+            string arguments = (" " + cmdvanish + " \"" + targetprgname + " " + audioparam + " " + URL + " && echo Completed download! && pause\"");
+            //Now to execute it
+            executeShellCommand("cmd.exe", arguments);
+        }
+        public void executeShellCommand(string command, string arguments)
+        {
+            //MessageBox.Show(command + arguments); //Debugging purposes
+            var p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = command;
+            p.StartInfo.Arguments = arguments;
+            p.Start();
+        }
+
     }
 }
